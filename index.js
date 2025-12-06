ce7eb7869812a68ea2c48ccfc2758352d5cdcef8
Joy <joykenneth605@gmail.com>
2025-12-06 21:20:00 +0100

    Add index page and partials folder

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
 require('dotenv').config();
@@ -7,7 +8,11 @@ app.set('view engine', 'ejs');
 const PORT = process.env.port || 3000;
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Weather App' });
+});
+
+app.get('/check', (req, res) => {
+    res.render('check', { title: 'Check Weather' });
 });
 
 app.listen(PORT, () => {
