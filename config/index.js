ab00a6751ce50321ebe7464365ea2a0fe0cbdbab
Joy <joykenneth605@gmail.com>
2025-12-07 19:23:59 +0100

    made minor changes to the check route and added the config folder

---
+++ config/index.js
@@ -0,0 +1,7 @@
+const envFound = require('dotenv').config()
+if(!envFound) throw new Error("Cannot find .env file")
+
+module.exports= {
+    port: process.env.PORT, 
+    openWeatherApiKey: process.env.OPENWEATHER_API_KEY
+}
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
 const express = require('express');
+const { port, openWeatherApiKey } = require('./config');
 const app = express();
-require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-const PORT = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Weather App' });
@@ -17,10 +15,38 @@ app.get('/check', (req, res) => {
     res.render('check', { title: 'Check Weather' });
 });
 
-app.post('/check', (req, res) => {
+app.post('/check', async (req, res) => {
     const { city, scale } = req.body;
+
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${openWeatherApiKey}&units=metric`;
+
+     try {
+        const response = await axios.get(url);
+        const data = response.data;
+
+        const today = new Date().toISOString().split("T")[0];
+
+        const todaysWeather = data.list.filter(entry =>
+            //get only today's weather
+            entry.dt_txt.startsWith(today)
+        );
+
+        res.status(200).json(todaysWeather);
+        
+        todaysWeather.map(entry => {
+            console.log("Time:", entry.dt_txt);
+            console.log("Temperature:", entry.main.temp + "°C");
+            console.log("Feels like:", entry.main.feels_like + "°C");
+            console.log("Humidity:", entry.main.humidity + "%");
+            console.log("Weather:", entry.weather[0].description);
+            console.log("Chance of Rain:", entry.pop);
+            console.log("---------------------------");
+        });
+    } catch (err) {
+        console.error("Error fetching weather:", err.response?.data || err.message);
+  }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
